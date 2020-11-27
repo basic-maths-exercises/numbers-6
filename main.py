ed5b8dff362dc28d1eec8fb90f069ca37d660e51
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-27 18:13:12 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def convertToHex( N ) :
+  myhex = 5*["0"]
+  # Your code goes here
+  
+  return myhex
+  
+for i in range(32) : 
+  print( "The number", i, "is", convertToHex(i), "in hex.")
