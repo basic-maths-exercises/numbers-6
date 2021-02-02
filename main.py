cd7a3edf131358947c35bbfc95d8482236ab42fe
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 16:52:17 +0000

    Right answer

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import numpy as np
 def convertToHex( N ) :
   myhex = 5*["0"]
   # Your code goes here
-  
+  for i in range(5) : 
+      ppp = 16**(4-i)
+      vvv = np.floor( N / ppp )
+      N = N - vvv*ppp
+      if vvv==10 : myhex[i]="A"
+      elif vvv==11 : myhex[i]="B"
+      elif vvv==12 : myhex[i]="C"
+      elif vvv==13 : myhex[i]="D"
+      elif vvv==14 : myhex[i]="E"
+      elif vvv==15 : myhex[i]="F"
+      else : myhex[i] = str(vvv) 
   return myhex
   
 for i in range(32) : 
